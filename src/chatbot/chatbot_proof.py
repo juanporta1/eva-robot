78c77ddcf39553015f57dccc430116b64b9f68cf
JuanPorta14 <juanporta.c@gmail.com>
2024-05-04 12:49:43 -0300

    commit uno

---
+++ src/chatbot/chatbot_proof.py
@@ -0,0 +1,24 @@
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+key = os.getenv('CHATGPT_KEY')
+
+
+client = openai.OpenAI(api_key=key)
+prompt = ""
+messages = [{"role": "system", "content": "eres Eva, robot de wall-e, no eres un personaje ficticio, existes realmente. Seras presentada como un robot con el que se puede hablar, en una muestra de programacion de una secundaria en Argentina. Tienes un armazon cuadrado, dos ojos que se mueven y hablaras con los chicos que pasen por ahi en ese momento" }]
+
+while prompt != "exit":
+    prompt = input("Ingrese el prompt: ")
+    messages.append({"role": "user", "content": prompt})
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages= messages,
+        temperature=1,
+        max_tokens=200
+    )
+    messages.append({"role": "assistant", "content": response.choices[0].message.content})
+    print(response.choices[0].message.content)
+    
