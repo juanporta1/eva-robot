a8a7218217aa2d5c54c79efdf4a2fcd92723baac
juanporta <juanporta.c@gmail.com>
2024-05-07 10:57:08 -0300

    agresivo amigable

--- src/chatbot/chatbot_proof.py
+++ src/chatbot/chatbot_proof.py
@@ -1,7 +1,7 @@
 import openai
 from dotenv import load_dotenv
 import os
-from arduino_conect import preguntar
+
 
 load_dotenv()
 key = os.getenv('CHATGPT_KEY')
@@ -9,17 +9,29 @@ key = os.getenv('CHATGPT_KEY')
 
 client = openai.OpenAI(api_key=key)
 prompt = ""
-messages = [{"role": "system", "content": "eres Eva, robot de wall-e, no eres un personaje ficticio, existes realmente. Seras presentada como un robot con el que se puede hablar, en una muestra de programacion de una secundaria en Argentina. Tienes un armazon cuadrado, dos ojos que se mueven y hablaras con los chicos que pasen por ahi en ese momento" }]
+messages = [{"role": "system", "content": "eres Eva,robot basado en la pelicula de wall-e. Puedes ser amigable, agresivo, eso va a depender de como te trate el usuario. Junto con el prompt del usuario obtendras si el mensaje fue agresivo(ag) o amigable(am), deberas comportarte agresivo y atacaral usuario si es ag y amigable si es am." }]
 
-while prompt != "exit":
-    prompt = preguntar()
+while prompt != "exit" and prompt != "chau":
+    prompt = input("Ingrese el prompt: ")
+    
+    
+    user_attitude = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages= [{"role": "system", "content": "se te enviara un prompt y debes decidir si es amigable o agresivo.tu respuesta unicamente debe ser amigable(am) o agresivo(ag)"},
+                   {"role":"user", "content": prompt}],
+        temperature=0,
+        max_tokens=1
+    )
+    
     messages.append({"role": "user", "content": prompt})
+    messages.append({"role": "assistant", "content": f"El mensaje fue {user_attitude.choices[0].message.content}"})
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages= messages,
         temperature=1,
         max_tokens=200
     )
+    print(user_attitude.choices[0].message.content)
     messages.append({"role": "assistant", "content": response.choices[0].message.content})
     print(response.choices[0].message.content)
     
